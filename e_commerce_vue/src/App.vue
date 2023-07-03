eefcff5dec055d1b3e03e65d5a2fe13baeab452c
Dimus <dislakoval@gmail.com>
2023-07-03 21:08:26 +0300

    Adding loading bar

--- e_commerce_vue/src/App.vue
+++ e_commerce_vue/src/App.vue
@@ -34,6 +34,10 @@
             </div>
         </nav>
 
+        <div class="is-loading-bar has-text-centered" v-bind:class="{ 'is-loading': $store.state.isLoading }">
+            <div class="lds-dual-ring"></div>
+        </div>
+
         <section class="section">
             <router-view />
         </section>
@@ -57,7 +61,7 @@ export default {
     beforeCreate() {
         this.$store.commit('initializeStore')
     },
-    mounted() { 
+    mounted() {
         this.cart = this.$store.state.cart
     },
     computed: {
@@ -75,4 +79,43 @@ export default {
 
 <style lang="scss">
 @import '../node_modules/bulma';
-</style>
+
+.lds-dual-ring {
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+}
+
+.lds-dual-ring:after {
+    content: " ";
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border-radius: 50%;
+    border: 6px solid #ccc;
+    border-color: #ccc transparent #ccc transparent;
+    animation: lds-dual-ring 1.2s linear infinite;
+}
+
+@keyframes lds-dual-ring {
+    0% {
+        transform: rotate(0deg);
+    }
+
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+.is-loading-bar {
+    height: 0;
+    overflow: hidden;
+
+    -webkit-transition: all 0.3s;
+    transition: all 0.3s;
+
+    &.is-loading {
+        height: 80px;
+    }
+}</style>
