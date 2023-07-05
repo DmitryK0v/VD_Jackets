3423cde3ce232c298217e15ab3dbab9cd117fc9b
Dimus <dislakoval@gmail.com>
2023-07-05 13:26:19 +0300

    Make LogIn possible

--- e_commerce_vue/src/App.vue
+++ e_commerce_vue/src/App.vue
@@ -74,6 +74,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
@@ -85,6 +87,14 @@ export default {
     },
     beforeCreate() {
         this.$store.commit('initializeStore')
+
+        const token = this.$store.state.token
+
+        if (token) {
+            axios.defaults.headers.common['Authorization'] = "Token " + token
+        } else {
+            axios.defaults.headers.common['Authorization'] = ""
+        }
     },
     mounted() {
         this.cart = this.$store.state.cart
