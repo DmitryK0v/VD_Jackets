da1750bf95ff8417c23fab6ddf5fa93b7a148aee
Dimus <dislakoval@gmail.com>
2023-07-05 12:20:08 +0300

    Added View Cart

--- e_commerce_vue/src/router/index.js
+++ e_commerce_vue/src/router/index.js
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import Product from '../views/Product.vue'
 import Category from '../views/Category.vue'
 import Search from '../views/Search.vue'
+import Cart from '../views/Cart.vue'
 
 
 const routes = [
@@ -25,6 +26,12 @@ const routes = [
     name: 'Search',
     component: Search
   },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  },
+
   {
     path: '/:category_slug/:product_slug/',
     name: 'Product',
